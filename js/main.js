9897030f65b29755a414dacabfa9b77553bb35c5
Tolihama <fra070@gmail.com>
2021-11-19 14:10:25 +0100

    Mail list generator

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 const app = new Vue({
     el: '#app',
+    data: {
+        apiURL: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        mailList: [],
+    },
+    created() {
+        this.mailListGenerator();
+    },
+    methods: {
+        mailListGenerator() {
+            for (let i = 0; i < 10; i++) {
+                axios.get(this.apiURL)
+                .then(response => {
+                    this.mailList.push(response.data.response);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            }
+        },
+    },
 });
